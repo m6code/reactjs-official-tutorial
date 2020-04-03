41cb06e789a00329060eb43ab34ef6c7a43ae6f9
M6code <benjamynfrank101@gmail.com>
2020-04-03 20:27:53 +0100

    Passing  Data Through Props

--- reactjs-official-tut/src/index.js
+++ reactjs-official-tut/src/index.js
@@ -6,55 +6,55 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+      );
+    }
   }
 
-  render() {
-    const status = 'Next player: X';
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i}/>;
+    }
 
-    return (
-      <div>
+    render() {
+      const status = 'Next player: X';
+
+      return (
+        <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
         </div>
-      </div>
-    );
-  }
-}
+        </div>
+        );
+      }
+    }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
+    class Game extends React.Component {
+      render() {
+        return (
+        <div className="game">
         <div className="game-board">
-          <Board />
+        <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      <div>{/* status */}</div>
+    <ol>{/* TODO */}</ol>
+    </div>
+    </div>
     );
   }
 }
@@ -64,4 +64,4 @@ class Game extends React.Component {
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
-);
+  );
